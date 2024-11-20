d30d2223f7a5a6b88efe759a7a2caf712d44a6d3
Trevor Holm <tholm@uci.edu>
2024-11-19 22:03:22 -0800

    Improved Crosswalk Detection accuracy

--- crosswalk_detection.py
+++ crosswalk_detection.py
@@ -3,7 +3,17 @@ from tkinter import filedialog, Toplevel, Label
 from PIL import Image, ImageTk
 import cv2
 
-model = YOLO("/Users/trevorholm/PycharmProjects/senior-design-gui-prototype/runs/detect/train/weights/best.pt")
+# model = YOLO("yolov8n.pt")
+#
+# model.train(
+#     data="/Users/trevorholm/PycharmProjects/senior-design-gui-prototype/CrosswalkDataset/data.yaml",  # Path to your YAML file
+#     epochs=5,                     # Number of training epochs
+#     imgsz=640,                     # Image size (pixels)
+#     batch=16,                      # Batch size
+#     workers=8                      # Number of workers for data loading
+# )
+
+model = YOLO("/Users/trevorholm/PycharmProjects/senior-design-gui-prototype/runs/detect/train4/weights/best.pt")
 
 def detect_crosswalk(image_path):
     results = model(image_path)
